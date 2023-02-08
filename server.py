a2b4fcb9cc00b08015359ee257f09d8be2f2b29a
Yury Lobkov <racoon.enot@gmail.com>
2023-02-08 17:48:59 -0300

    404page

--- server.py
+++ server.py
@@ -11,7 +11,10 @@ def my_home():
 
 @app.route("/<string:page_name>")
 def html_page(page_name):
-    return render_template(page_name)
+    try:
+        return render_template(page_name)
+    except:
+        return render_template('/404.html')
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
